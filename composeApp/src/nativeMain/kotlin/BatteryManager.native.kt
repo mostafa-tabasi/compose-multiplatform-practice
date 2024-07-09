bf251fd0cc9d49273e8e631471e43b9a91ba0935
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-07-09 16:21:47 +0800

    feat: Implemented battery manager in iOS

---
+++ composeApp/src/nativeMain/kotlin/BatteryManager.native.kt
@@ -0,0 +1,11 @@
+import platform.UIKit.UIDevice
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class BatteryManager {
+    actual fun getBatteryLevel(): Int {
+        UIDevice.currentDevice.batteryMonitoringEnabled = true
+        val batteryLevel = UIDevice.currentDevice.batteryLevel
+
+        return (batteryLevel * 100).roundToInt()
+    }
+}
