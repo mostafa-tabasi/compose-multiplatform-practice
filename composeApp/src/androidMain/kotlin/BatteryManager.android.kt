e49d17762a3d33f942458e6681ff13ce043a7d1e
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-07-09 13:33:50 +0800

    feat: Implemented battery manager in Android

---
+++ composeApp/src/androidMain/kotlin/BatteryManager.android.kt
@@ -0,0 +1,20 @@
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import kotlin.math.roundToInt
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class BatteryManager(
+    private val context: Context,
+) {
+    actual fun getBatteryLevel(): Int {
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { iFilter ->
+            context.registerReceiver(null, iFilter)
+        }
+
+        val level: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val scale: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+        return (level * 100 / scale.toFloat()).roundToInt()
+    }
+}
