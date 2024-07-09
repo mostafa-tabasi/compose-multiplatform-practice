eeffa5a8f3988aff2bc269569afc6864885b38f5
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-07-09 16:38:04 +0800

    feat: Used battery manager class

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,3 +1,8 @@
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(
+        batteryManager = remember { BatteryManager() }
+    )
+}
