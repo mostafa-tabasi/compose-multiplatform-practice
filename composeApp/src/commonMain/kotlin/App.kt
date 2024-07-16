a3452f7d55dd0036bed8b8ebb75db554c8eb2466
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-07-16 12:27:40 +0800

    feat: Added and used some resources

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,20 @@
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import composemultiplatformpractice.composeapp.generated.resources.Res
+import composemultiplatformpractice.composeapp.generated.resources.compose_multiplatform
+import composemultiplatformpractice.composeapp.generated.resources.current_battery_level
+import composemultiplatformpractice.composeapp.generated.resources.platform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -13,12 +23,16 @@ fun App(
     batteryManager: BatteryManager,
 ) {
     MaterialTheme {
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center,
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text("The current battery level is ${batteryManager.getBatteryLevel()}")
+            Image(painterResource(Res.drawable.compose_multiplatform), null)
+            Text(stringResource(Res.string.platform, getPlatform().name))
+            Text(stringResource(Res.string.current_battery_level, batteryManager.getBatteryLevel()))
         }
     }
 }
